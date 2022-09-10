91a6eeedc31a0e4d81f6cc4b4b3172b7d548962b
Alexander <valverin95@gmail.com>
2022-09-10 19:33:20 +0300

    ✨ created drop zone for deleting Notes

--- client/src/app/pages/main/home/home/home.component.ts
+++ client/src/app/pages/main/home/home/home.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { NoteService } from '@app/core/services';
 import { ProfileService } from '@app/core/services/profile/profile.service';
+import { CdkDragEnd, CdkDragMove } from '@angular/cdk/drag-drop';
+import { NoteDragState } from './note-card/note-card.component';
 
 @Component({
   selector: 'app-home',
@@ -9,8 +11,13 @@ import { ProfileService } from '@app/core/services/profile/profile.service';
 })
 export class HomeComponent implements OnInit {
 
+  @ViewChild('dropZone')
+  dropZone!: ElementRef<HTMLElement>;
+
   profile$ = this.profileService.get();
   notes$ = this.noteService.getAll();
+
+  isDraggedToDropZone = false;
 
   constructor(
     private noteService: NoteService,
@@ -18,5 +25,23 @@ export class HomeComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+  }
+
+  onNoteMoved($event: NoteDragState<CdkDragMove>) {
+    const point = $event.event.pointerPosition;
+    const elements = document.elementsFromPoint(point.x, point.y);
+    const isDropZone = !!elements.find(el => this.dropZone.nativeElement === el);
+
+    $event.component.inDropZone = isDropZone;
+
+    if (this.isDraggedToDropZone !== isDropZone) {
+      this.isDraggedToDropZone = isDropZone;
+    }
+  }
+
+  onNoteDropped($event: NoteDragState<CdkDragEnd>) {
+    this.isDraggedToDropZone = false;
+    $event.component.inDropZone = false;
+    console.log($event);
   }
 }
